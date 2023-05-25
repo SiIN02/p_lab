5a587b5c932948c52373db7bfc059c7f51bb79b4
jhoon6 <33897117+jhoon6@users.noreply.github.com>
2023-05-25 21:28:23 +0900

    Update streamer_main.py

--- stream_mjpeg/streamer_main.py
+++ stream_mjpeg/streamer_main.py
@@ -1,14 +1,3 @@
-# -*- encoding: utf-8 -*-
-#-------------------------------------------------#
-# Date created          : 2020. 8. 18.
-# Date last modified    : 2020. 8. 19.
-# Author                : chamadams@gmail.com
-# Site                  : http://wandlab.com
-# License               : GNU General Public License(GPL) 2.0
-# Version               : 0.1.0
-# Python Version        : 3.6+
-#-------------------------------------------------#
-
 from server import app
 
 if __name__ == '__main__' :
